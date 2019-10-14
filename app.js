881b837274d6c4ccd1f3f31c9d4e30ceb5451c38
govtbodies <tom.ignarri@gmail.com>
2019-10-14 12:52:14 -0400

    flashcard display made responsive

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/public'));
 app.set("view engine", "ejs");
 
@@ -10,15 +12,19 @@ var flashcards = [
 ];
 
 
+
+app.get('/', (req, res) => res.render("home"));
+
+
 // Post to an input action
-app.post("/newFlashcard", function(req, res){
+app.post("/newFlashcard", function(req, res) {
     var question = req.body.question;
     var translation = req.body.translation;
     var newFlashcard = {question: question, translation: translation};
     flashcards.push(newFlashcard);
+    res.redirect("/");
+    console.log(flashcards);
 });
 
-
-app.get('/', (req, res) => res.render("home"));
 
 app.listen(3000, () => console.log("Flashcard app is listening"));
